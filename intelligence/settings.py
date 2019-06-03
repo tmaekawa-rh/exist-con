443e64be3d512355e518b58c8397f0f936ee626e
Tadayoshi Maekawa <tmaekawa@redhat.com>
2019-06-04 00:54:23 +0900

    modified settings.py

--- intelligence/settings.py
+++ intelligence/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '&ug=2p3t^x+&!9vt*kjr--g-)ta5=o)4@v&6ks-w#k69nl+v$k'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '.3d6a.kepcodevops.openshiftapps.com'
+    '.kepcodevops.openshiftapps.com'
 ]
 
 
